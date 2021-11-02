4288bd3ae109a2f4d4d57555325b88a7b823e512
ndi-odoo <ndi@odoo.com>
2021-11-02 19:17:57 +0000

    add odoo_academy module-again

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary':"""Academy app""",
+    
+    'description': """
+       my description""",
+    'author': 'ndi',
+    'category': 'Training',
+    'version' : '0.1',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'demo': [
+        
+    ]
+    
+}
