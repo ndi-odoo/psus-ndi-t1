d476b81ae23e65f4298efd3bca9765c82ba890b3
ndi-odoo <ndi@odoo.com>
2021-11-02 16:45:56 +0000

    first commit odoo academy

---
+++ odoo_academy2/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,30 @@
+# _*- coding: utf-8 -*-
+
+{
+  'name': 'Odoo Academy',
+    
+  'summary': """Academy app to manage training""",
+    
+  'description': """
+      Academy module to manage Training
+      -courses
+      -sessions
+      -attendess
+  
+  """,
+  'author':'nidiaz',
+    
+  'category': 'Training',
+    
+  'version': '0.1',
+    
+  'depends': ['base'],
+    
+  'data': [
+      
+  ],  
+  'demo': [
+      
+  ]  
+    
+}
