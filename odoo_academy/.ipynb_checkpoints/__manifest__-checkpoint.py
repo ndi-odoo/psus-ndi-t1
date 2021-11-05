c1c84cd6271ab8780ce202082b2dd9b6e00c2857
ndi-odoo <ndi@odoo.com>
2021-11-05 18:27:49 +0000

    add views and corrected menuitems

--- odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -21,7 +21,8 @@
     'data': [
         'security/academy_security.xml',
         'security/ir.model.access.csv',
-        'views/academy_menuitems.xml'
+        'views/academy_menuitems.xml',
+        'views/course_views'
     ],
     'demo': [
         'demo/academy_demo.xml'
